e3149e1b4b4a0912f38cc5865809b590431ebc12
majie <mjyyl@126.com>
2018-01-24 18:34:26 +0800

    add koa error handle

--- src/controller/sign.js
+++ src/controller/sign.js
@@ -1,18 +1,50 @@
 import { UserModel } from "../model/index";
+import validator from 'validator';
 
 class Sign {
-  signup (ctx, next) {
+  async signup (ctx, next) {
     let userVO = ctx.request.body;
-    console.log(userVO);
     let user = new UserModel();
     user.loginid = userVO.loginid;
     user.name = userVO.name;
     user.password = userVO.password;
     user.email = userVO.email;
-    user.save().then(() => {
-      console.log("signup successful")
-    ctx.body = "signup successful";
-  } ).catch(e => console.log(e));
+
+    //1. 验证
+// 1.1 验证注册id
+    const exp = /^[a-zA-Z_][a-zA-Z0-9_]+$/i;
+
+    //2. 保存用户
+
+
+    await user.save().catch((err) => {
+ctx.throw(500,err);
+    });
+    let result = {};
+
+
+
+ctx.body = "signup successful";
+
+
+    // await user.save().then(() => {
+    //   console.log("signup successful");
+    //   ctx.body = "signup successful";
+    // }).catch((err) => {
+    //   console.log(err);
+    //   ctx.status = err.status || 500;
+    //   ctx.body = err.message;
+    // });
+
+    // console.log(rst);
+    //
+    // rst.then(() => {
+    //   ctx.body = "signup successful";
+    // }).catch((err) => {
+    //   console.log(err);
+    //   ctx.status = err.status || 500;
+    //   ctx.body = err.message;
+    // });
   }
 
   signin(ctx, next) {
--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import router from './router';
 
 const app = new Koa();
 
+
 app.use(bodyParser());
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    // will only respond with JSON
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+      message: err.message
+    };
+  }
+});
+
 app.use(router.routes());
 
 app.listen(3000);
